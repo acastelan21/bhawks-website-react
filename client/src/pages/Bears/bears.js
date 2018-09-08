00677f25faee5137d8b0fdc812040961d79f9f18
AlCast <alvaro.castelan.21@gmail.com>
2018-09-07 23:17:21 -0500

    added seasonCard component + started bears page

--- client/src/pages/Bears/bears.js
+++ client/src/pages/Bears/bears.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
-import ComingSoon from "../../components/ComingSoon";
+import SeasonCard from "../../components/SeasonCard";
 export class Bears extends Component {
+   
+  
   render() {
     return (
       <div className="page bears">
-        <ComingSoon/>
+      <div className="row">
+        <div className="col-6">
+          <SeasonCard
+          image={"https://www.bleachernation.com/bears/wp-content/uploads/2017/04/chicago-bears-face-logo-new-feature.jpg"}
+          date = {"2017-2018"}
+          link={"/2017-2018"}
+          />  
+        </div>
+        <div className="col-6">
+          <SeasonCard
+          image={"https://www.bleachernation.com/bears/wp-content/uploads/2017/04/chicago-bears-face-logo-new-feature.jpg"}
+          date = {"2018-2019"}
+          link={"/2018-2019"}
+
+          />
+        </div>
+      </div>
+        
+        
+       
         
       </div>
     )
