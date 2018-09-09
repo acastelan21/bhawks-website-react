db846bbf66c2ae6755c723f6d97481f3056b8bec
AlCast <alvaro.castelan.21@gmail.com>
2018-09-08 19:17:11 -0500

    set up mongodb connection + started set up to connect data to front end

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
-// Serve up static assets (usually on heroku)
+const PORT = process.env.PORT || 3001;
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+const db= process.env.MONGODB_URI || "mongodb://localhost:27017/chicago-sports-gifs";
+mongoose.connect(db, function(error){
+  if (error){
+    console.error(error);
+  }
+  else{
+    console.log("Mongo connection is successful");
+  }
+})
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Send every request to the React app
-// Define any API routes before this runs
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
